6aad06c4b0092aa324cac3a0bcd41f249ad537f0
Sar Malik <malik@sarthak.ca>
2021-05-20 13:06:49 -0400

    app:x11:firefox:migrate to monorepo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t firefox:alpine -f dockerfile .
-
-set +x;
--- firefox.sh
+++
@@ -1,7 +0,0 @@
-# ------------
-# x11docker:firefox:alpine
-# ------------
-
-x11docker -p --home firefox:alpine
-
-set +x;
